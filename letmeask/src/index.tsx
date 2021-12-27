727f5935cb4ed2a6770db74617d58e0c83e2d284
thatielys <thatielys@DESKTOP-17GLA63.localdomain>
2021-12-27 20:26:46 -0300

    Exemplo de estado, Button.tsx

---
+++ letmeask/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Button } from './components/Button';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <Button />
+    <Button />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
