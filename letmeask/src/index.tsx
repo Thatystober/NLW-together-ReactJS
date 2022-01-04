88ead55b361b5285f40d4955b1cd6acaa658a6a2
thatielys <thatielys@DESKTOP-17GLA63.localdomain>
2022-01-04 17:45:17 -0300

    Criacao de duas telas, e instalacao do react-router-dom

--- letmeask/src/index.tsx
+++ letmeask/src/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './services/firebase'
-import { Button } from './components/Button';
+import './services/firebase';
+
+import './styles/global.scss'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Button />
-    <Button />
   </React.StrictMode>,
   document.getElementById('root')
 );
